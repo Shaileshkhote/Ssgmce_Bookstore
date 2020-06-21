d17d5c130bcc640305b39196c5575094dc4a3ca0
Shailesh Khote <shaileshkhote@gmail.com>
2020-06-21 14:46:13 +0530

    github commit

---
+++ app/src/main/java/com/comparedost/ssgmce_bookstore/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.comparedost.ssgmce_bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
