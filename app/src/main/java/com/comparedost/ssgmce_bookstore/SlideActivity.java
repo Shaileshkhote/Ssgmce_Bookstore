193ee5c592e97d828953bb048f36ed3bd0a653ec
sajal gupta <sajalgupta198@gmail.com>
2020-07-07 16:12:12 +0530

    onboarding issue is solve

--- app/src/main/java/com/comparedost/ssgmce_bookstore/SlideActivity.java
+++ app/src/main/java/com/comparedost/ssgmce_bookstore/SlideActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class SlideActivity extends AppCompatActivity {
+
     public static ViewPager viewPager;
     SlideViewPagerAdapter adapter;
 
@@ -16,28 +17,10 @@ public class SlideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide);
 
-        viewPager=findViewById(R.id.viewpager);
-        adapter=new SlideViewPagerAdapter(this);
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new SlideViewPagerAdapter(this);
         viewPager.setAdapter(adapter);
 
-        if(!(isOpenAlready()))
-        {
-            Intent intent=new Intent(SlideActivity.this,LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }
-        else
-        {
-            SharedPreferences.Editor editor=getSharedPreferences("slide",MODE_PRIVATE).edit();
-            editor.putBoolean("slide",true);
-            editor.commit();
 
-        }
-    }
-
-    private boolean isOpenAlready() {
-        SharedPreferences sharedPreferences=getSharedPreferences("slide",MODE_PRIVATE);
-        boolean result=sharedPreferences.getBoolean("slide",false);
-        return result;
     }
 }
--- app/src/main/java/com/comparedost/ssgmce_bookstore/SlideViewPagerAdapter.java
+++ app/src/main/java/com/comparedost/ssgmce_bookstore/SlideViewPagerAdapter.java
@@ -2,7 +2,6 @@ package com.comparedost.ssgmce_bookstore;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +12,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
-import static android.view.LayoutInflater.*;
-
 public class SlideViewPagerAdapter extends PagerAdapter {
+
     Context ctx;
-    public SlideViewPagerAdapter(Context ctx){
-        this.ctx=ctx;
+
+    public SlideViewPagerAdapter(Context ctx) {
+        this.ctx = ctx;
     }
+
     @Override
     public int getCount() {
         return 3;
@@ -34,38 +34,42 @@ public class SlideViewPagerAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, final int position) {
         LayoutInflater layoutInflater= (LayoutInflater) ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.layout.slide_screen,container,false);
+        View view=layoutInflater.inflate(R.layout.slide_screen,container,false);
 
 
         ImageView logo=view.findViewById(R.id.logo);
+
         ImageView ind1=view.findViewById(R.id.ind1);
         ImageView ind2=view.findViewById(R.id.ind2);
         ImageView ind3=view.findViewById(R.id.ind3);
+
 
         TextView title=view.findViewById(R.id.title);
         TextView desc=view.findViewById(R.id.desc);
 
         ImageView next=view.findViewById(R.id.next);
         ImageView back=view.findViewById(R.id.back);
+
         Button btnGetStarted=view.findViewById(R.id.btnGetStarted);
         btnGetStarted.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(ctx,MainActivity.class);
+            public void onClick(View v) {
+                Intent intent=new Intent(ctx,LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
                 ctx.startActivity(intent);
             }
         });
         next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                com.comparedost.ssgmce_bookstore.SlideActivity.viewPager.setCurrentItem(position+1);
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position+1);
             }
         });
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                com.comparedost.ssgmce_bookstore.SlideActivity.viewPager.setCurrentItem(position-1);
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position-1);
             }
         });
 
@@ -77,8 +81,8 @@ public class SlideViewPagerAdapter extends PagerAdapter {
                 ind2.setImageResource(R.drawable.unselected);
                 ind3.setImageResource(R.drawable.unselected);
 
-                title.setText("Buy Books");
-                desc.setText("you can buy your semester book here");
+                title.setText("Shoping Place");
+                desc.setText("This is random text taking from lorem ipsum tesing puspose");
                 back.setVisibility(View.GONE);
                 next.setVisibility(View.VISIBLE);
                 break;
@@ -88,33 +92,38 @@ public class SlideViewPagerAdapter extends PagerAdapter {
                 ind2.setImageResource(R.drawable.selected);
                 ind3.setImageResource(R.drawable.unselected);
 
-                title.setText("Sell Books");
-                desc.setText("you can sell your semester book here");
+                title.setText("Shopping on the way");
+                desc.setText("This is random text taking from lorem ipsum tesing puspose");
                 back.setVisibility(View.VISIBLE);
                 next.setVisibility(View.VISIBLE);
                 break;
-
             case 2:
                 logo.setImageResource(R.drawable.log3);
                 ind1.setImageResource(R.drawable.unselected);
                 ind2.setImageResource(R.drawable.unselected);
                 ind3.setImageResource(R.drawable.selected);
 
-                title.setText("Make your choise");
-                desc.setText("learn and enjoy");
+                title.setText("Pay on Delivery");
+                desc.setText("This is random text taking from lorem ipsum tesing puspose");
                 back.setVisibility(View.VISIBLE);
                 next.setVisibility(View.GONE);
                 break;
 
+
+
+
+
+
         }
+
 
         container.addView(view);
         return view;
+
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView((View) object);
-
     }
 }
