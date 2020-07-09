9f5f21a45d364c2c4ec7af0b27759475119573e5
Nilesh Wanjari <nileshwanjari24@gmail.com>
2020-07-09 15:19:13 +0530

    My_cart layout. In this cart layout we have use recycleview ,button to go forward for checkout and total amount will be shown in the side of the continue button.

--- app/src/main/java/com/comparedost/ssgmce_bookstore/my_cart.java
+++ app/src/main/java/com/comparedost/ssgmce_bookstore/my_cart.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class checkout extends AppCompatActivity {
+public class my_cart extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
